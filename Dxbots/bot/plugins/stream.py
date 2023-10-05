2b3d593d82d1441fbabd119e857424a9bfa88256
Shashankls <125437933+Shashankls@users.noreply.github.com>
2023-10-05 23:44:09 +0530

    Update stream.py

--- Dxbots/bot/plugins/stream.py
+++ Dxbots/bot/plugins/stream.py
@@ -104,8 +104,8 @@ async def private_receive_handler(c: Client, m: Message):
             text=msg_text.format(get_name(log_msg), humanbytes(get_media_file_size(m)), online_link, stream_link),
             quote=True,
             disable_web_page_preview=True,
-            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("STREAM🖥", url=stream_link), #Stream Link
-                                                InlineKeyboardButton("DOWNLOAD📥", url=online_link)]]) #Download Link
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("STREAM🖥",url=online_link ), #Stream Link
+                                                InlineKeyboardButton("DOWNLOAD📥",url=stream_link )]]) #Download Link
         )
     except FloodWait as e:
         print(f"Sleeping for {str(e.x)}s")
