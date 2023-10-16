4e3d56e7f66a3664b5cd4727f42b52a705efc0e2
Shashankls <125437933+Shashankls@users.noreply.github.com>
2023-10-16 21:26:52 +0530

    Update vars.py

--- Dxbots/vars.py
+++ Dxbots/vars.py
@@ -8,9 +8,9 @@ load_dotenv()
 
 class Var(object):
     MULTI_CLIENT = False
-    API_ID = int(getenv('API_ID'))
-    API_HASH = str(getenv('API_HASH'))
-    BOT_TOKEN = str(getenv('BOT_TOKEN'))
+    API_ID = environ.get('API_ID',"11760418") 
+    API_HASH = environ.get('API_HASH',"1087bd9fc871216be0e86287e5c50ac3") 
+    BOT_TOKEN = environ.get('BOT_TOKEN',"6463692735:AAEGPGQuP4tIUwJ7FSBay8Y52wCW9MUFpU8")
     name = str(getenv('name', 'filetolinkbot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
@@ -18,10 +18,10 @@ class Var(object):
     PORT = int(getenv('PORT', 8080))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "").split())  
+    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "1904953726").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
-    OWNER_USERNAME = str(getenv('OWNER_USERNAME'))
+    OWNER_USERNAME = environ.get('OWNER_USERNAME',"Uuuuiioopg")
     if 'DYNO' in environ:
         ON_HEROKU = True
         APP_NAME = str(getenv('APP_NAME'))
@@ -36,6 +36,6 @@ class Var(object):
     else:
         #URL = "https://{}/".format(FQDN)
         URL = FQDN 
-    DATABASE_URL = str(getenv('DATABASE_URL'))
+    DATABASE_URL = environ.get('DATABASE_URL',"mongodb+srv://Shashanklssss:shashank.ls1324@cluster0.ctzkp4x.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp")
     UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', None))
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split())) 
