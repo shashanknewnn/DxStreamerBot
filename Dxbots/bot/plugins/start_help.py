f3d4e08d9086005d90ebd1f383a9bb59975cccd9
shashanknewnn <148247675+shashanknewnn@users.noreply.github.com>
2024-03-25 14:53:02 +0530

    Update start_help.py

--- Dxbots/bot/plugins/start_help.py
+++ Dxbots/bot/plugins/start_help.py
@@ -1,1 +1,139 @@
-#mm
+# Copyright (C) 2024 DX-MODS
+#Licensed under the  MIT License;
+#you may not use this file except in compliance with the License.
+#Author ZIYAN
+#if you use our codes try to donate here https://www.buymeacoffee.com/ziyankp
+
+from Dxbots.bot import DxStreamBot
+from Dxbots.vars import Var
+import logging
+logger = logging.getLogger(__name__)
+from Dxbots.bot.plugins.stream import MY_PASS
+from Dxbots.utils.human_readable import humanbytes
+from Dxbots.utils.database import Database
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.errors import UserNotParticipant
+from Dxbots.utils.file_properties import get_name, get_hash, get_media_file_size
+db = Database(Var.DATABASE_URL, Var.name)
+from pyrogram.types import ReplyKeyboardMarkup
+
+if MY_PASS:
+            buttonz=ReplyKeyboardMarkup(
+            [
+                ["start⚡️","help📚","login🔑","DC"],
+                ["Subscribe ❤️","ping📡","status📊","maintainers😎"]
+
+            ],
+            resize_keyboard=True
+        )
+else:
+            buttonz=ReplyKeyboardMarkup(
+            [
+                ["start⚡️","help📚","DC"],
+                ["Subscribe ❤️","ping📡","status📊","maintainers😎"]
+
+            ],
+            resize_keyboard=True
+        )
+
+
+
+@DxStreamBot.on_message((filters.command("start") | filters.regex('start⚡️')) & filters.private )
+async def start(b, m):
+    if not await db.is_user_exist(m.from_user.id):
+        await db.add_user(m.from_user.id)
+        await b.send_message(
+            Var.BIN_CHANNEL,
+            f"**Nᴇᴡ Usᴇʀ Jᴏɪɴᴇᴅ:** \n\n__Mʏ Nᴇᴡ Fʀɪᴇɴᴅ__ [{m.from_user.first_name}](tg://user?id={m.from_user.id}) __Sᴛᴀʀᴛᴇᴅ Yᴏᴜʀ Bᴏᴛ !!__"
+        )
+    if Var.UPDATES_CHANNEL != "None":
+        try:
+            user = await b.get_chat_member(Var.UPDATES_CHANNEL, m.chat.id)
+            if user.status == "kicked":
+                await b.send_message(
+                    chat_id=m.chat.id,
+                    text="__𝓢𝓞𝓡𝓡𝓨, 𝓨𝓞𝓤 𝓐𝓡𝓔 𝓐𝓡𝓔 𝓑𝓐𝓝𝓝𝓔𝓓 𝓕𝓡𝓞𝓜 𝓤𝓢𝓘𝓝𝓖 𝓜𝓔. 𝓒ᴏɴᴛᴀᴄᴛ ᴛʜᴇ Developer__\n\n  **𝙃𝙚 𝙬𝙞𝙡𝙡 𝙝𝙚𝙡𝙥 𝙮𝙤𝙪**",
+                    disable_web_page_preview=True
+                )
+                return
+        except UserNotParticipant:
+             await DxStreamBot.send_photo(
+                chat_id=m.chat.id,
+                photo="https://graph.org/file/d6d33c2a6d99e2632565f.jpg",
+                caption="<i>𝙹𝙾𝙸𝙽 CHANNEL 𝚃𝙾 𝚄𝚂𝙴 𝙼𝙴🔐</i>",
+                reply_markup=InlineKeyboardMarkup(
+                    [
+                        [
+                            InlineKeyboardButton("Jᴏɪɴ ɴᴏᴡ 🔓", url=f"https://t.me/{Var.UPDATES_CHANNEL}")
+                        ]
+                    ]
+                ),
+
+            )
+             return
+        except Exception:
+            await b.send_message(
+                chat_id=m.chat.id,
+                text="<i>Something went wrong</i> <b> <a href='https://t.me/Rockerssupport_bot'>CLICK HERE FOR SUPPORT </a></b>",
+
+                disable_web_page_preview=True)
+            return
+    await DxStreamBot.send_photo(
+        chat_id=m.chat.id,
+        photo ="https://graph.org/file/b6baf804c256853494257.jpg",
+        caption =f'Hi {m.from_user.mention(style="md")}!,\nI am Telegram File to Link Generator Bot with Channel support.\nSend me any file and get a direct download link and streamable link.!',
+        reply_markup=buttonz)
+
+
+@DxStreamBot.on_message((filters.command("help") | filters.regex('help📚')) & filters.private )
+async def help_handler(bot, message):
+    if not await db.is_user_exist(message.from_user.id):
+        await db.add_user(message.from_user.id)
+        await bot.send_message(
+            Var.BIN_CHANNEL,
+            f"**Nᴇᴡ Usᴇʀ Jᴏɪɴᴇᴅ **\n\n__Mʏ Nᴇᴡ Fʀɪᴇɴᴅ__ [{message.from_user.first_name}](tg://user?id={message.from_user.id}) __Started Your Bot !!__"
+        )
+    if Var.UPDATES_CHANNEL != "None":
+        try:
+            user = await bot.get_chat_member(Var.UPDATES_CHANNEL, message.chat.id)
+            if user.status == "kicked":
+                await bot.send_message(
+                    chat_id=message.chat.id,
+                    text="<i>Sᴏʀʀʏ Sɪʀ, Yᴏᴜ ᴀʀᴇ Bᴀɴɴᴇᴅ FROM USING ᴍᴇ. CONTACT THE DEVELOPER</i> ",
+
+                    disable_web_page_preview=True
+                )
+                return
+        except UserNotParticipant:
+            await DxStreamBot.send_photo(
+                chat_id=message.chat.id,
+                photo="https://graph.org/file/b6baf804c256853494257.jpg",
+                Caption="**𝙹𝙾𝙸𝙽 𝚂𝚄𝙿𝙿𝙾𝚁𝚃 𝙶𝚁𝙾𝚄𝙿 𝚃𝙾 𝚄𝚂𝙴 ᴛʜɪs Bᴏᴛ!**\n\n__Dᴜᴇ ᴛᴏ Oᴠᴇʀʟᴏᴀᴅ, Oɴʟʏ Cʜᴀɴɴᴇʟ Sᴜʙsᴄʀɪʙᴇʀs ᴄᴀɴ ᴜsᴇ ᴛʜᴇ Bᴏᴛ!__",
+                reply_markup=InlineKeyboardMarkup(
+                    [
+                        [
+                            InlineKeyboardButton("🤖 Jᴏɪɴ Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ", url=f"https://t.me/{Var.UPDATES_CHANNEL}")
+                        ]
+                    ]
+                ),
+
+            )
+            return
+        except Exception:
+            await bot.send_message(
+                chat_id=message.chat.id,
+                text="__Sᴏᴍᴇᴛʜɪɴɢ ᴡᴇɴᴛ Wʀᴏɴɢ. Cᴏɴᴛᴀᴄᴛ ᴍᴇ__ [Support](https://t.me/Rockerssupport_bot).",
+                disable_web_page_preview=True)
+            return
+    await message.reply_text(
+        text="""<b> Send me any file or video i will give you streamable link and download link.</b>\n
+<b> I also support Channels, add me to you Channel and send any media files and see miracle✨ also send /list to know all commands""",
+
+        disable_web_page_preview=True,
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [InlineKeyboardButton("💁‍♂️ Owner", url="https://t.me/Rockerssupport_bot")],
+                [InlineKeyboardButton("💥 For Source Code", url="https://t.me/Rockerssupport_bot")]
+            ]
+        )
